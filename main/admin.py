62c5697b78f18a06fd41467f942ad44367ff5ab8
douno <aboubacar.douno@gn.ehealthafrica.org>
2020-06-02 06:47:49 +0100

    Removed slug fields

--- main/admin.py
+++ main/admin.py
@@ -4,44 +4,44 @@ from main.models import *
 # Register your models here.
 @admin.register(AdminLevel)
 class AdminLevelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'parent_admin_level', 'slug',)
+    list_display = ('name', 'parent_admin_level',)
 
 
 @admin.register(MeasurementUnit)
 class MeasurementUnitAdmin(admin.ModelAdmin):
-    list_display = ('name', 'shortname', 'parent_unit', 'is_active', 'slug',)
+    list_display = ('name', 'shortname', 'parent_unit', 'is_active',)
 
 
 @admin.register(FacilityType)
 class FacilityTypeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug',)
+    list_display = ('name',)
 
 
 @admin.register(ItemType)
 class ItemTypeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug',)
+    list_display = ('name',)
 
 
 @admin.register(TaskType)
 class TaskTypeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug',)
+    list_display = ('name',)
 
 
 @admin.register(CustomerType)
 class CustomerTypeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug',)
+    list_display = ('name',)
 
 
 @admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
-    list_display = ('name', 'shortname', 'start_date', 'end_date', 'slug',)
+    list_display = ('name', 'shortname', 'start_date', 'end_date',)
 
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'shortname', 'item_type', 'program', 'quantity', 'unit_of_measurement', 'base_unit_of_measurement', 'slug',)
+    list_display = ('name', 'shortname', 'item_type', 'program', 'quantity', 'unit_of_measurement', 'base_unit_of_measurement',)
 
 
 @admin.register(Lag)
 class LagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'on_time_duration', 'on_time_type', 'unreported_duration', 'unreported_type', 'slug',)
+    list_display = ('name', 'on_time_duration', 'on_time_type', 'unreported_duration', 'unreported_type',)
--- main/models.py
+++ main/models.py
@@ -3,40 +3,25 @@ from django.utils.text import slugify
 
 # Create your models here.
 class AdminLevel(models.Model):
-    # TODO: Should we add a slug
     name = models.CharField(max_length=150, unique=True)
     parent_admin_level = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
-    slug = models.SlugField(max_length=150, editable=False)
 
     def __str__(self):
         return self.name
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super(AdminLevel, self).save(*args, **kwargs)
-
 
 class MeasurementUnit(models.Model):
-    # TODO: Should we add a slug
     name = models.CharField(max_length=150, unique=True)
     shortname = models.CharField(max_length=150, unique=True)
     parent_unit = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
     is_active = models.BooleanField(default=True)
-    slug = models.SlugField(max_length=150, editable=False)
 
     def __str__(self):
         return self.name
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.shortname)
-        super(MeasurementUnit, self).save(*args, **kwargs)
-
 
 class TypeBaseModel(models.Model):
     name = models.CharField(max_length=150, unique=True)
-    slug = models.SlugField(max_length=150, editable=False)
 
     def __str__(self):
         return self.name
@@ -46,50 +31,28 @@ class TypeBaseModel(models.Model):
 
 
 class FacilityType(TypeBaseModel):
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super(FacilityType, self).save(*args, **kwargs)
+    pass
 
 
 class ItemType(TypeBaseModel):
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super(ItemType, self).save(*args, **kwargs)
+    pass
 
 
 class TaskType(TypeBaseModel):
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super(TaskType, self).save(*args, **kwargs)
+    pass
 
 
 class CustomerType(TypeBaseModel):
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super(CustomerType, self).save(*args, **kwargs)
+    pass
 
 
 class Location(models.Model):
     name = models.CharField(max_length=150)
     parent_location = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)
     admin_level = models.ForeignKey(AdminLevel, on_delete=models.SET_NULL, null=True, blank=True)
-    slug = models.SlugField(max_length=150, editable=False)
 
     def __str__(self):
         return self.name
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.shortname)
-        super(Location, self).save(*args, **kwargs)
 
 
 class Program(models.Model):
@@ -97,15 +60,9 @@ class Program(models.Model):
     shortname = models.CharField(max_length=150, unique=True)
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
-    slug = models.SlugField(max_length=150, editable=False)
 
     def __str__(self):
         return self.name
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.shortname)
-        super(Program, self).save(*args, **kwargs)
 
 
 class Item(models.Model):
@@ -117,15 +74,10 @@ class Item(models.Model):
     # Note: Do we really need the two below, maybe one will suffice
     unit_of_measurement = models.ForeignKey(MeasurementUnit, on_delete=models.SET_NULL, null=True, related_name='uom_items')
     base_unit_of_measurement = models.ForeignKey(MeasurementUnit, on_delete=models.SET_NULL, null=True, related_name='base_uom_items')
-    slug = models.SlugField(max_length=150, editable=False)
 
     def __str__(self):
         return self.name
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.shortname)
-        super(Item, self).save(*args, **kwargs)
 
 
 class Lag(models.Model):
@@ -140,12 +92,6 @@ class Lag(models.Model):
     unreported_duration = models.IntegerField(default=1)
     unreported_type = models.CharField(max_length=10, choices=LAG_TIME_TYPE_CHOICES)
     is_active = models.BooleanField(default=True)
-    slug = models.SlugField(max_length=150, editable=False)
 
     def __str__(self):
         return self.name
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super(Lag, self).save(*args, **kwargs)
